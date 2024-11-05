94137e124b928401bfb0114d5e76ba73b62753fa
Varun Singh <varunsingh00712@gmail.com>
2024-11-05 02:27:00 -0800

    fixes

--- src/lib/singerData.js
+++ src/lib/singerData.js
@@ -1,100 +1,56 @@
 import lineup_image_1 from "@/assets/images/home-1/lineup-kathak.jpg";
-import lineup_image_2 from "@/assets/images/home-1/lineup-music-sitar.jpeg";
-import lineup_image_3 from "@/assets/images/home-1/lineup-music-flute.jpg";
+import lineup_image_2 from "@/assets/images/home-1/lineup-music.jpeg";
+import lineup_image_3 from "@/assets/images/home-1/lineup-ramp.webp";
+import lineup_image_4 from "@/assets/images/home-1/lineup-image-2.jpg";
 import lineup_image_5 from "@/assets/images/home-1/lineup-nova.jpg";
 import lineup_image_6 from "@/assets/images/home-1/lineup-aerreo.jpg";
-import lineup_image_7 from "@/assets/images/home-1/lineup-music-tabla.jpeg";
 
 export const singerData = [
-  {
-    id: 2,
-    name: "Megha Rawoot",
-    genere: "Folk Music - Sitar",
-    img: lineup_image_2,
-    socil_link: [
-      {
-        id: 1,
-        icon: "f",
-        link: "",
-      },
-      {
-        id: 2,
-        icon: "f",
-        link: "https://www.instagram.com/megharawoot/",
-      },
-      {
-        id: 3,
-        icon: "f",
-        link: "",
-      },
-      {
-        id: 4,
-        icon: "f",
-        link: "",
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: "Kartikeya Vashist",
-    genere: "Folk Music - Flute",
-    img: lineup_image_3,
-    socil_link: [
-      {
-        id: 1,
-        icon: "f",
-        link: "",
-      },
-      {
-        id: 2,
-        icon: "f",
-        link: "https://www.instagram.com/kartikeyavashist/",
-      },
-      {
-        id: 3,
-        icon: "f",
-        link: "",
-      },
-      {
-        id: 4,
-        icon: "f",
-        link: "",
-      },
-    ],
-  },
-  {
-    id: 4,
-    name: "Divyanshu Kumar",
-    genere: "Folk Music - Tabla",
-    img: lineup_image_7,
-    socil_link: [
-      {
-        id: 1,
-        icon: "f",
-        link: "",
-      },
-      {
-        id: 2,
-        icon: "f",
-        link: "https://www.instagram.com/divyanshu.tabla/",
-      },
-      {
-        id: 3,
-        icon: "f",
-        link: "",
-      },
-      {
-        id: 4,
-        icon: "f",
-        link: "",
-      },
-    ],
-  },
   {
     id: 1,
     name: "Kathak Fusion",
     genere: "Dance",
     img: lineup_image_1,
+  },
+  {
+    id: 2,
+    name: "Folk Music",
+    genere: "MUSIC",
+    img: lineup_image_2,
+  },
+  {
+    id: 3,
+    name: "Tradational Costume Ramp Walk ",
+    genere: "FAISHON",
+    img: lineup_image_3,
+  },
+  {
+    id: 4,
+    name: "Rahgir",
+    genere: "Soulful",
+    img: lineup_image_4,
+    socil_link: [
+      {
+        id: 1,
+        icon: "f",
+        link: "https://www.facebook.com/rahgirlive/",
+      },
+      {
+        id: 2,
+        icon: "f",
+        link: "https://www.instagram.com/rahgirlive",
+      },
+      {
+        id: 3,
+        icon: "f",
+        link: "https://www.youtube.com/channel/UCeVu3bE1SprTkpcVgBCyM9g",
+      },
+      {
+        id: 4,
+        icon: "f",
+        link: "https://open.spotify.com/artist/1CGbcdGB3aFFUCjaJimwER",
+      },
+    ],
   },
   {
     id: 5,
